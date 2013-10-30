cebcaf227c88489e74b6878edfce269680c5b7b4
victor <victor@victor-HP-Mini-110-3100>
2013-10-30 16:55:51 -0300

    Novo DAO Functionary

--- AssistenciaTecnica/src/br/com/assistenciaTecnica/base/functionary/Role.java
+++ AssistenciaTecnica/src/br/com/assistenciaTecnica/base/functionary/Role.java
@@ -11,16 +11,21 @@ import br.com.assistenciaTecnica.base.object.BasicEntity;
 @Entity @Table(name = "tb_role")
 public class Role extends BasicEntity
 {
+	@Column(columnDefinition = "VARCHAR(30)")
+	private String name;
 	@Column(columnDefinition = "FLOAT")
 	private Float salary;
-
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	public Float getSalary() {
 		return salary;
 	}
-
 	public void setSalary(Float salary) {
 		this.salary = salary;
-	}
-	
-	
+	}	
 }
--- AssistenciaTecnica/src/br/com/assistenciaTecnica/dao/specific/DAOFunctionary.java
+++ AssistenciaTecnica/src/br/com/assistenciaTecnica/dao/specific/DAOFunctionary.java
@@ -1,6 +1,5 @@
 package br.com.assistenciaTecnica.dao.specific;
 
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -9,106 +8,27 @@ import br.com.assistenciaTecnica.base.functionary.Functionary;
 import br.com.assistenciaTecnica.base.functionary.Role;
 import br.com.assistenciaTecnica.base.functionary.Scholarity;
 import br.com.assistenciaTecnica.base.person.Person;
-import br.com.assistenciaTecnica.base.person.Telephone;
 import br.com.assistenciaTecnica.base.user.User;
 import br.com.assistenciaTecnica.dao.generic.DAOPerson;
+import br.com.assistenciaTecnica.dao.generic.TypedQuery;
 
 public class DAOFunctionary extends DAOPerson implements IDAOFunctionary 
 {
-
 	@Override
-	public List<Person> searchByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Person> searchByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Person> searchByBirthday(Calendar birthday) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Person> searchByTelephones(List<Telephone> telephones) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Person> searchByStreet(String street) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Person> searchByStreetNumber(String streetNumber) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Person> searchByDistric(String distric) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Person> searchByStreetType(Integer streetTypeID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Person> searchByRegion(Integer regionID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Person> searchByCountry(Integer countryID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Person> seeAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void insert(Person object) {
-		// TODO Auto-generated method stub
+	public List<Functionary> searchByRole(String roleName) {
+		try
+		{
+			String jpql = "SELECT f FROM Funcionario f WHERE p.role.name LIKE :roleName";
+			TypedQuery<Funcionario> query = entityManager.createQuery(jpql, Funcionario.class);
+			query.setParameter("roleName", roleName);
+			
+			return query.getResultList();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 		
-	}
-
-	@Override
-	public void refresh(Person object) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void remove(Person object) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Person queryById(Integer key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Functionary> searchByRole(Role role) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
